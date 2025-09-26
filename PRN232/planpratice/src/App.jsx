0c2691c9b512f49128e6a5b9c7dd0c3d4b5c36ba
AmayaKuro <vhtkiet9615@gmail.com>
2025-09-26 09:00:49 +0700

    init

--- PRN232/planpratice/src/App.jsx
+++ PRN232/planpratice/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Login from './app/pages/Login';
 import Register from './app/pages/Register';
 import Profile from './app/pages/Profile';
+import Landing from './app/pages/Landing';
 import { Routes, Route, Navigate } from 'react-router';
 import { AuthAPI } from './app/components/APIService/AuthAPI';
 
@@ -21,29 +22,35 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route 
-          path="/login" 
+        <Route
+          path="/"
+          element={
+            <Landing />
+          }
+        />
+        <Route
+          path="/login"
           element={
             <PublicRoute>
               <Login />
             </PublicRoute>
-          } 
+          }
         />
-        <Route 
-          path="/register" 
+        <Route
+          path="/register"
           element={
             <PublicRoute>
               <Register />
             </PublicRoute>
-          } 
+          }
         />
-        <Route 
-          path="/profile" 
+        <Route
+          path="/profile"
           element={
             <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
-          } 
+          }
         />
         <Route path="/" element={<Navigate to="/" replace />} />
         <Route path="*" element={<Navigate to="/profile" replace />} />
