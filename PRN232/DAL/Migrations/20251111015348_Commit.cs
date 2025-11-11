4d5724ae5dae81aad6bbada2463f6fde2d3eb23c
AmayaKuro <vhtkiet9615@gmail.com>
2025-11-11 08:54:49 +0700

    full send to reset migrate

---
+++ PRN232/DAL/Migrations/20251111015348_Commit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Commit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuizOTPAccesses");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QuizOTPAccesses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OTPId = table.Column<int>(type: "integer", nullable: false),
+                    StudentId = table.Column<int>(type: "integer", nullable: false),
+                    AccessedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizOTPAccesses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuizOTPAccesses_QuizOTPs_OTPId",
+                        column: x => x.OTPId,
+                        principalTable: "QuizOTPs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuizOTPAccesses_Users_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizOTPAccess_OTP_Student",
+                table: "QuizOTPAccesses",
+                columns: new[] { "OTPId", "StudentId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizOTPAccesses_StudentId",
+                table: "QuizOTPAccesses",
+                column: "StudentId");
+        }
+    }
+}
